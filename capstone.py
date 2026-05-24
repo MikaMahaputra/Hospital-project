a217c6fddb283ac933aacb84f621e37363f2aeee
Mika Mahaputra <mikamahaputra@yahoo.co.id>
2026-05-24 23:52:06 +0700

    Added validations and confirmation

--- capstone.py
+++ capstone.py
@@ -24,7 +24,55 @@ patient_data = [
     }
 ]
  
+import csv
+import os
 running = True
+
+def patient_id():
+    while True:
+        patient_id_input = input("Insert patient's ID: (id001 for example)").strip()
+        if not patient_id_input:
+            print("Patient ID cannot be empty!")
+        elif any(p['patient_id'] == patient_id_input for p in patient_data):
+            print(f"ID '{patient_id_input}' is already taken!")
+        else:
+            return patient_id_input
+
+def patient_name():
+    while True:
+        name = input("Insert patient's name: ").strip()
+        if not name:
+            print("Name cannot be empty!")
+        else:
+            return name
+        
+def patient_age():
+    while True:
+        try:
+            age = int(input("Insert patient's age: "))
+            if age <= 0:
+                print("Age must be more than zero.")
+            else:
+                return age
+        except ValueError:
+            print("Invalid age, try again.")
+
+
+def patient_gender():
+    while True:
+        gender = input("Specify patient's gender (Male/Female): ").strip().capitalize()
+        if gender not in ["Male", "Female"]:
+            print("Please try again, gender must be Male/Female.")
+        else:
+            return gender
+        
+def patient_diagnosis():
+    while True:
+        diagnosis = input("Insert patient's diagnosis: ").strip()
+        if not diagnosis:
+            print("Diagnosis cannot be empty!")
+        else:
+            return diagnosis
 
 def view_menu():
     print("\n======================================")
@@ -37,11 +85,11 @@ def view_menu():
     print("======================================")
 
 def add_patient():
-    patient_id_add  = input("Insert patient's ID: ")
-    name_add        = input("Insert patient's name: ")
-    age_add         = int(input("Insert patient's age: "))
-    gender_add      = input("Specify patient's gender (Male/Female): ")
-    diagnosis_add   = input("Insert patient's diagnosis: ")
+    patient_id_add  = patient_id()
+    name_add        = patient_name()
+    age_add         = patient_age()
+    gender_add      = patient_gender()
+    diagnosis_add   = patient_diagnosis()
 
     patient_data.append({
         'patient_id': patient_id_add,
@@ -94,14 +142,19 @@ def delete_patient():
 
     if 0 <= index < len(patient_data):
         removed = patient_data.pop(index)
-        print(f"\nPatient '{removed['name']}' successfully deleted!")
-        view_patient()
+        delete_confirm = input(f"\nAre you sure you want to delete '{removed['name']}' (ID: {removed['patient_id']})? (y/n): ").strip().lower()
+        if delete_confirm == "y":
+            patient_data.pop(index)
+            print(f"\nPatient '{removed['name']}' successfully deleted!")
+            view_patient()
+        else:
+            print("\n Cancelling deletion")
     else:
         print("Invalid input, please try again.")
 
 while running:
     view_menu()
-    menu = int(input("Please input a menu number:\n"))
+    menu = int(input("Please input a menu number: "))
     if menu == 1:
         add_patient()
     elif menu == 2:
